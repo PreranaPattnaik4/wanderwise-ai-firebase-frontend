76c36857321d8c1233077255cafbca0c41933f95
Prerana Kailash Pattnaik <preranapattnaik4@gmail.com>
2025-10-09 16:41:52 +0000

    make very small

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -27,7 +27,7 @@ export default function Home() {
         <Hero />
         <div id="onboarding" className="container mx-auto px-4 pb-12 pt-16">
           <div className="z-10 text-center">
-            <h2 className="text-3xl font-headline font-bold mb-4">Where are you headed? Let’s craft your perfect plan!</h2>
+            <h2 className="text-2xl font-headline font-bold mb-4">Where are you headed? Let’s craft your perfect plan!</h2>
             <QuickPlannerForm setItinerary={setItinerary} />
           </div>
           
